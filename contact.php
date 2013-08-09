375fd65caf912b65fd7d796581dee2b3277d3827
Tyler Strayhan <tylerkstrayhan@gmail.com>
2013-08-08 19:13:42 -0700

    All files for Azuru website.
    This is all the source for Azuru's website that includes images, HTML,
    CSS and a contact form in PHP.

---
+++ contact.php
@@ -0,0 +1,36 @@
+<?php
+
+if(!$_POST) exit;
+
+$email = $_POST['email'];
+
+
+if(!eregi("^[a-z0-9]+([_\\.-][a-z0-9]+)*" ."@"."([a-z0-9]+([\.-][a-z0-9]+)*)+"."\\.[a-z]{2,}"."$",$email )){
+	$error.="Invalid email address entered";
+	$errors=1;
+}
+if($errors==1) echo $error;
+else{
+	$values = array ('name','email','message');
+	$required = array('name','email','message');
+	 
+	$your_email = "us@azuru.net";
+	$email_subject = "New Message: ".$_POST['subject'];
+	$email_content = "new message:\n";
+	
+	foreach($values as $key => $value){
+	  if(in_array($value,$required)){
+		if ($key != 'subject' && $key != 'company') {
+		  if( empty($_POST[$value]) ) { echo 'PLEASE FILL IN REQUIRED FIELDS'; exit; }
+		}
+		$email_content .= $value.': '.$_POST[$value]."\n";
+	  }
+	}
+	 
+	if(@mail($your_email,$email_subject,$email_content)) {
+		echo 'Message sent!'; 
+	} else {
+		echo 'ERROR!';
+	}
+}
+?>
